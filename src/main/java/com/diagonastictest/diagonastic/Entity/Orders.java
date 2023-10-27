aa3a18648a6d37ea7b97228af07de5ea05e34786
Sajid18May <sajid1852002@gmail.com>
2023-10-27 22:25:47 +0530

    mapping in order table

--- src/main/java/com/diagonastictest/diagonastic/Entity/Orders.java
+++ src/main/java/com/diagonastictest/diagonastic/Entity/Orders.java
@@ -1,14 +1,10 @@
 package com.diagonastictest.diagonastic.Entity;
 
 import java.util.Date;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Entity
